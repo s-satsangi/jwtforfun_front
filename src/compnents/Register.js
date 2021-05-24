f5ba3ee36f9e4455ba9d12b40af5275197bf065f
s-satsangi <s.satsangi@gmail.com>
2021-05-23 21:21:03 -0400

    set up Register component

--- src/compnents/Register.js
+++ src/compnents/Register.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React {useState} from "react";
+import axios from "axios";
 
 const Register = () => {
-  return <div></div>;
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("")
+    
+    const registerUser = (e) => {
+        e.preventDefault()
+        axios.get("http://localhost:3000/newuser").then(res=>res.json()).then(res=>console.log(res))
+        console.log("Register fetch goes here")
+    }
+
+    const handleChange = (e, setHook) => {
+        setHook(e.target.value)
+    }
+
+  return <div>
+      <form onSubmit={registerUser}>
+        <input 
+            type="text"
+            value={login}
+            onChange={(e)=>handleChange(e,setLogin)}
+        />
+        <input 
+            type="text"
+            value={password}
+            onChange={(e)=>handleChange(e,setPassword)}
+        />
+        <input 
+            type="submit"
+            value="Register New User"
+        />
+      </form>
+  </div>;
 };
 
 export default Register;
